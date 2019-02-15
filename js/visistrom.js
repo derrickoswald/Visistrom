2f095d9f2b87ac98982d7eafaf9d09cf84ca6f34
Derrick Oswald <derrick.oswald@9code.ch>
2019-02-15 23:21:33 +0100

    convert to dates

--- js/visistrom.js
+++ js/visistrom.js
@@ -12,7 +12,11 @@ requirejs
         {
             function show (objects)
             {
-                alert (JSON.stringify (objects, null, 4).substring (0, 2000));
+                var new_objects = {};
+                for (var property in objects)
+                    if (objects.hasOwnProperty (property))
+                        new_objects[new Date (property)] = objects[property];
+                alert (JSON.stringify (new_objects, null, 4).substring (0, 2000));
             }
             read_data.load ("data/4weeks_MD_T1_MHF1.csv").then (show);
         };
