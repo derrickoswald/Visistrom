87a090ef5f0151e6c051acd9b5edeeb70affb4fd
Derrick Oswald <derrick.oswald@9code.ch>
2019-02-15 22:56:57 +0100

    read in the sample data

--- js/visistrom.js
+++ js/visistrom.js
@@ -1,20 +1,28 @@
 
 /**
- * Main javascript file for Open Energy Data Hack Days Visstrom application
+ * Main javascript file for Open Energy Data Hack Days Vis1strom application
  */
 "use strict";
 requirejs
 (
-    [],
-    function ()
+    ["read_data"],
+    function (read_data)
     {
+        function doit ()
+        {
+            function show (objects)
+            {
+                alert (JSON.stringify (objects, null, 4).substring (0, 2000));
+            }
+            read_data.load ("data/4weeks_MD_T1_MHF1.csv").then (show);
+        };
         // initialize widgets
-        document.getElementById ("light-bulb-icon").onclick = function () { alert ("hello world"); };
+        document.getElementById ("light-bulb-icon").onclick = doit;
         document.getElementById ("forecast").onclick = function () { alert ("forecast"); };
         document.getElementById ("club").onclick     = function () { alert ("club"); };
         document.getElementById ("history").onclick  = function () { alert ("history"); };
         document.getElementById ("options").onclick  = function () { alert ("options"); };
 
-//        cimmap.initialize ();
+        read_data
     }
 );
